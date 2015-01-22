eb77585ce494626c2c9057dad9cb3e8c11263350
Евгений <elizavetaskull@gmail.com>
2015-01-22 16:57:07 +0600

    rename as relise

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.clojars.khoda.lein-expectations "0.0.9-SNAPSHOT"
+(defproject org.clojars.khoda.lein-expectations "0.0.9"
   :description "Leiningen plugin to run tests written using the expectations library."
   :url "https://github.com/gar3thjon3s/lein-expectations"
-  :dependencies [[org.clojars.khoda.expectations "2.0.13-SNAPSHOT"]]
+  :dependencies [[org.clojars.khoda.expectations "2.0.13"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :expectations/show-finished-ns true
